c557faa1fba9eb5fecfa91077356fed24f3433b8
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-03-21 10:50:01 -0600

    Coding CKKS

---
+++ Homomorphic ciphers/Ckks.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Research.SEAL;
+
+namespace Homomorphic_ciphers;
+
+public class Ckks
+{
+    private readonly Encryptor _encryptor;
+    private readonly Decryptor _decryptor;
+    public Evaluator Evaluator { get; }
+    private readonly CKKSEncoder _encoder;
+    private readonly double _scale;
+
+    public Ckks()
+    {
+        var context = GenerateContext();
+        GenerateKeys(context, out var publicKey, out var secretKey);
+        _scale = Math.Pow(2.0, 40);
+        _encryptor = new Encryptor(context, publicKey);
+        _decryptor = new Decryptor(context, secretKey);
+        Evaluator = new Evaluator(context);
+        _encoder = new CKKSEncoder(context);
+    }
+
+    public Ckks(SEALContext context, PublicKey publicKey, SecretKey secretKey, double scale = 1099511627776)
+    {
+        _scale = scale;
+        _encryptor = new Encryptor(context, publicKey);
+        _decryptor = new Decryptor(context, secretKey);
+        Evaluator = new Evaluator(context);
+        _encoder = new CKKSEncoder(context);
+    }
+
+    public static SEALContext GenerateContext()
+    {
+        EncryptionParameters encryptionParameters = new(SchemeType.CKKS);
+        const ulong polyModulusDegree = 16384;
+        encryptionParameters.PolyModulusDegree = polyModulusDegree;
+        encryptionParameters.CoeffModulus = CoeffModulus.Create(
+            polyModulusDegree, [60, 40, 40, 60]);
+
+        return new SEALContext(encryptionParameters);
+    }
+
+    public static void GenerateKeys(SEALContext context, out PublicKey publicKey, out SecretKey secretKey)
+    {
+        KeyGenerator keyGenerator = new(context);
+        secretKey = keyGenerator.SecretKey;
+        keyGenerator.CreatePublicKey(out publicKey);
+    }
+
+    public Ciphertext Encrypt(long plainData)
+    {
+        var plain = new Plaintext();
+        _encoder.Encode(plainData, _scale, plain);
+        var encrypted = new Ciphertext();
+        _encryptor.Encrypt(plain, encrypted);
+        return encrypted;
+    }
+
+    public long Decrypt(Ciphertext encryptedData)
+    {
+        var plain = new Plaintext();
+        _decryptor.Decrypt(encryptedData, plain);
+        ICollection<double> plainData = [];
+        _encoder.Decode(plain, plainData);
+        return (long) Math.Round(plainData.First());
+    }
+}
---
+++ Validation tests/Homomorphic ciphers/CkksTests.cs
@@ -0,0 +1,132 @@
+﻿using Homomorphic_ciphers;
+using Microsoft.Research.SEAL;
+
+namespace Validation_tests.Homomorphic_ciphers;
+
+public class CkksTests
+{
+    [Fact]
+    public void TestCase1()
+    {
+        Ckks ckks = new();
+        const long plainData = 5;
+
+        var encryptedData = ckks.Encrypt(plainData);
+        var decryptedData = ckks.Decrypt(encryptedData);
+
+        Assert.Equal(plainData, decryptedData);
+    }
+
+    [Fact]
+    public void TestCase2()
+    {
+        var context = Ckks.GenerateContext();
+        Ckks.GenerateKeys(context, out var publicKey, out var secretKey);
+        Ckks ckks = new(context, publicKey, secretKey);
+        var plainData = Random.Shared.NextInt64(2048);
+
+        var encryptedData = ckks.Encrypt(plainData);
+        long decryptedData = ckks.Decrypt(encryptedData),
+            result = Math.Abs(decryptedData - plainData);
+
+        Assert.True(result <= 5);
+    }
+
+    /// <summary>
+    /// Test the homomorphic addition property of the CKKS cryptosystem
+    /// </summary>
+    [Fact]
+    public void TestCase3()
+    {
+        var context = Ckks.GenerateContext();
+        Ckks.GenerateKeys(context, out var publicKey, out var secretKey);
+        Ckks ckks = new(context, publicKey, secretKey);
+        long a = Random.Shared.NextInt64(1024),
+            b = Random.Shared.NextInt64(1024),
+            addition = a + b;
+
+        Ciphertext encryptedA = ckks.Encrypt(a),
+            encryptedB = ckks.Encrypt(b),
+            encryptedAddition = new();
+        var evaluator = ckks.Evaluator;
+        evaluator.Add(encryptedA, encryptedB, encryptedAddition);
+
+        long decryptedData = ckks.Decrypt(encryptedAddition),
+            result = Math.Abs(addition - decryptedData);
+
+        Assert.True(result <= 5);
+    }
+
+    /// <summary>
+    /// Test the homomorphic subtraction property of the CKKS cryptosystem
+    /// </summary>
+    [Fact]
+    public void TestCase4()
+    {
+        var context = Ckks.GenerateContext();
+        Ckks.GenerateKeys(context, out var publicKey, out var secretKey);
+        Ckks ckks = new(context, publicKey, secretKey);
+        long a = Random.Shared.NextInt64(1024),
+            b = Random.Shared.NextInt64(1024),
+            subtraction = a - b;
+
+        Ciphertext encryptedA = ckks.Encrypt(a),
+            encryptedB = ckks.Encrypt(b),
+            encryptedSubtraction = new();
+        var evaluator = ckks.Evaluator;
+        evaluator.Sub(encryptedA, encryptedB, encryptedSubtraction);
+
+        long decryptedData = ckks.Decrypt(encryptedSubtraction),
+            result = Math.Abs(subtraction - decryptedData);
+
+        Assert.True(result <= 5);
+    }
+
+    /// <summary>
+    /// Test the homomorphic multiplication property of the CKKS cryptosystem
+    /// </summary>
+    [Fact]
+    public void TestCase5()
+    {
+        var context = Ckks.GenerateContext();
+        Ckks.GenerateKeys(context, out var publicKey, out var secretKey);
+        Ckks ckks = new(context, publicKey, secretKey);
+        long a = Random.Shared.NextInt64(1024),
+            b = Random.Shared.NextInt64(1024),
+            multiplication = a * b;
+
+        Ciphertext encryptedA = ckks.Encrypt(a),
+            encryptedB = ckks.Encrypt(b),
+            encryptedMultiplication = new();
+        var evaluator = ckks.Evaluator;
+        evaluator.Multiply(encryptedA, encryptedB, encryptedMultiplication);
+
+        long decryptedData = ckks.Decrypt(encryptedMultiplication),
+            result = Math.Abs(multiplication - decryptedData);
+
+        Assert.True(result <= 5);
+    }
+
+    /// <summary>
+    /// Test the homomorphic square power property of the CKKS cryptosystem
+    /// </summary>
+    [Fact]
+    public void TestCase6()
+    {
+        var context = Ckks.GenerateContext();
+        Ckks.GenerateKeys(context, out var publicKey, out var secretKey);
+        Ckks ckks = new(context, publicKey, secretKey);
+        long a = Random.Shared.NextInt64(1024),
+            square = a * a;
+
+        Ciphertext encryptedA = ckks.Encrypt(a),
+            encryptedSquare = new();
+        var evaluator = ckks.Evaluator;
+        evaluator.Square(encryptedA, encryptedSquare);
+
+        long decryptedData = ckks.Decrypt(encryptedSquare),
+            result = Math.Abs(square - decryptedData);
+
+        Assert.True(result <= 5);
+    }
+}
