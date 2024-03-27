667646b67abce85a0723bc70f87d3865db821784
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-03-27 15:14:17 -0600

    Coding SCrypt

---
+++ Hashes/SCrypt.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+using static Org.BouncyCastle.Crypto.Generators.SCrypt;
+
+namespace Hashes;
+
+public class SCrypt(byte[]? salt = null, int costParameter = 16384, int blockSize = 8, int parallelizationParameter = 1, int derivedKeyLength = 64)
+{
+    private readonly byte[] _salt = salt ?? RandomNumberGenerator.GetBytes(16);
+    public byte[] Hash(byte[] input)
+    {
+        return Generate(input, _salt, costParameter, blockSize, parallelizationParameter, derivedKeyLength);
+    }
+}
