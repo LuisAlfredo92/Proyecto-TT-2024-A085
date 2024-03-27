f2a8108c9cb7ad9554729e4793e747789d150172
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-03-27 15:14:24 -0600

    Coding BCrypt

---
+++ Hashes/BCrypt.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+namespace Hashes;
+
+public class BCrypt(byte[]? salt = null, int cost = 4)
+{
+    private readonly byte[] _salt = salt ?? RandomNumberGenerator.GetBytes(16);
+    public byte[] Hash(byte[] input)
+    {
+        return Org.BouncyCastle.Crypto.Generators.BCrypt.Generate(input, _salt, cost);
+    }
+}
---
+++ Validation tests/Hashes/BCryptTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using Hashes;
+
+namespace Validation_tests.Hashes;
+
+public class BCryptTests
+{
+    [Fact]
+    public void TestCase1()
+    {
+        var plainData = new byte[Random.Shared.Next(72)];
+        var hash = new BCrypt().Hash(plainData);
+        Assert.True(hash.Length == 24);
+    }
+}
