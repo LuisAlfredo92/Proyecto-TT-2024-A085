9e2c2b6485a7d771894b2b167c10daaa71432c04
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-03-28 11:59:37 -0600

    Coding names generator

---
+++ Data generators/Identifying data/NamesGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Identifying_data;
+
+public class NamesGenerator
+{
+    private readonly string[] _lastNames;
+    private readonly string[] _names;
+
+    public NamesGenerator()
+    {
+        CsvConfiguration csvConfiguration = new(CultureInfo.InvariantCulture)
+        {
+            Encoding = Encoding.UTF8,
+        };
+
+        // Last names
+        List<string> lastNames = [];
+        using var lastNamesStreamReader = File.OpenText("apellidos.csv");
+        using var lastNamesReader = new CsvReader(lastNamesStreamReader, csvConfiguration);
+        while (lastNamesReader.Read())
+            lastNames.Add(lastNamesReader.GetField(0));
+        _lastNames = [.. lastNames];
+
+        // Men names
+        using var menStreamReader = File.OpenText("hombres.csv");
+        List<string> names = [];
+        using var menReader = new CsvReader(menStreamReader, csvConfiguration);
+        menReader.Read();
+        while (menReader.Read())
+            names.Add(menReader.GetField(0));
+
+        // Women names
+        using var womenStreamReader = File.OpenText("mujeres.csv");
+        using var womenReader = new CsvReader(womenStreamReader, csvConfiguration);
+        womenReader.Read();
+        while (womenReader.Read())
+            names.Add(womenReader.GetField(0));
+
+        _names = [.. names];
+    }
+
+    public string GetRandomName()
+        => $"{_names[Random.Shared.Next(_names.Length)]} {_lastNames[Random.Shared.Next(_lastNames.Length)]} {_lastNames[Random.Shared.Next(_lastNames.Length)]}";
+}
