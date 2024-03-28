50a9505515eb9a30c19e668fa10438c884528760
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-03-28 13:18:35 -0600

    Coding CURPs generator

---
+++ Data generators/Identifying data/Curps/CurpsGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace Identifying_data.Curps;
+
+public class CurpsGenerator
+{
+    private static readonly string[] States =
+    [
+        "AS", "BC", "BS", "CC", "CS", "CH", "CL", "CM", "DF", "DG", "GT", "GR", "HG", "JC", "MC", "MS", "MN", "NT",
+        "NL", "OC", "PL", "QT", "QR", "SP", "SL", "SR", "TC", "TS", "TL", "VZ", "YN", "ZS", "NE"
+    ];
+
+    private static readonly char[] Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYS".ToCharArray();
+    private static readonly char[] Consonants = "BCDFGHJKLMNPQRSTVWXYZ".ToCharArray();
+
+    public static string Generate()
+    {
+        var curp = new StringBuilder();
+        curp.Append(GenerateName());
+        curp.Append(GenerateBornDate());
+        curp.Append(GenerateSex());
+        curp.Append(GenerateState());
+        curp.Append(GenerateConsonants());
+        curp.Append(GenerateHomoclave());
+        return curp.ToString();
+    }
+
+    private static string GenerateName()
+    {
+        var nameStringBuilder = new StringBuilder();
+        for (var i = 0; i < 4; i++)
+            nameStringBuilder.Append(Letters[Random.Shared.Next(0, Letters.Length)]);
+
+        return nameStringBuilder.ToString();
+    }
+
+    private static string GenerateBornDate()
+    {
+        var year = Random.Shared.Next(0, 100).ToString("00");
+        var month = Random.Shared.Next(1, 13).ToString("00");
+        var day = Random.Shared.Next(1, 29).ToString("00");
+        return $"{year}{month}{day}";
+    }
+
+    private static string GenerateSex()
+        => Random.Shared.Next(0, 2) == 1 ? "H" : "M";
+
+    private static string GenerateState()
+        => States[Random.Shared.Next(0, States.Length)];
+
+    private static string GenerateConsonants()
+    {
+        var consonantsStringBuilder = new StringBuilder();
+        for (var i = 0; i < 3; i++)
+            consonantsStringBuilder.Append(Consonants[Random.Shared.Next(0, Consonants.Length)]);
+
+        return consonantsStringBuilder.ToString();
+    }
+
+    private static string GenerateHomoclave()
+        => (Random.Shared.Next(0, 2) == 1 ? "A" : "0") + Random.Shared.Next(0, 10);
+}
--- Validation tests/Data generators/IdentifyingDataTests.cs
+++ Validation tests/Data generators/IdentifyingDataTests.cs
@@ -1,10 +1,12 @@
-﻿using Identifying_data;
+﻿using Identifying_data.Curps;
+using Identifying_data.Names;
 using Xunit.Abstractions;
 
 namespace Validation_tests.Data_generators;
 
-public class IdentifyingDataTests(ITestOutputHelper testOutputHelper)
+public class IdentifyingDataFacts(ITestOutputHelper testOutputHelper)
 {
+    // Test names generator
     [Fact]
     public void TestNamesGenerator()
     {
@@ -17,4 +19,19 @@ public class IdentifyingDataTests(ITestOutputHelper testOutputHelper)
             testOutputHelper.WriteLine(name);
         }
     }
+
+    // Test CURP generator
+    [Fact]
+    public void TestCurpsGenerator()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var curp = CurpsGenerator.Generate();
+            Assert.NotNull(curp);
+            Assert.NotEmpty(curp);
+            Assert.Equal(18, curp.Length);
+            testOutputHelper.WriteLine(curp);
+        }
+    }
+
 }
