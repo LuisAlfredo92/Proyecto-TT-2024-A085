dc4e83fb0e122cae2ff8ea57766528edde511a34
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-04-02 17:12:16 -0600

    Coding email generator

---
+++ Digital data/Email/EmailGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Digital_data.Email;
+
+public class EmailGenerator
+{
+    public static string GenerateEmail()
+    {
+        StringBuilder email = new();
+        int domainLength = Random.Shared.Next(12, 256),
+            localLength = Random.Shared.Next(5, 60);
+        for (var i = 0; i < domainLength; i++)
+            email.Append((char)Random.Shared.Next(97, 123));
+        email.Append('@');
+        for (var i = 0; i < localLength; i++)
+            email.Append((char)Random.Shared.Next(97, 123));
+        email.Append('.');
+        for (var i = 0; i < 3; i++)
+            email.Append((char)Random.Shared.Next(97, 123));
+        return email.ToString();
+    }
+}
---
+++ Validation tests/Data generators/DigitalDataTests.cs
@@ -0,0 +1,25 @@
+﻿using Digital_data.Email;
+using Xunit.Abstractions;
+
+namespace Validation_tests.Data_generators;
+
+public class DigitalDataTests(ITestOutputHelper testOutputHelper)
+{
+    // Test email generator
+    [Fact]
+    public void TestEmailGenerator()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var email = EmailGenerator.GenerateEmail();
+            var divided = email.Split('@');
+            Assert.NotNull(email);
+            Assert.NotEmpty(email);
+            Assert.Contains('@', email);
+            Assert.Contains('.', email);
+            Assert.True(divided[0].Length <= 255);
+            Assert.True(divided[1].Length <= 64);
+            testOutputHelper.WriteLine(email);
+        }
+    }
+}
