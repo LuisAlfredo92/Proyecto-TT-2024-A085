e6a4a8e87605f9dbad4944e22b07b1ae30a32aed
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-04-19 09:38:51 -0600

    CCT generator

---
+++ Data generators/Academic data/Cct/CctGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Academic_data.Cct;
+
+public class CctGenerator
+{
+    private static readonly char[] SchoolType = ['E', 'D', 'K'];
+    private static readonly string[] EducationalServiceType = ["CC", "JN", "PB", "PR", "ES", "SN", "TV", "ST", "IN"];
+    public static string GenerateCct()
+    {
+        var cct = new StringBuilder();
+        cct.Append(Random.Shared.Next(1, 33).ToString("D2"));
+        cct.Append(SchoolType[Random.Shared.Next(0, SchoolType.Length)]);
+        cct.Append(EducationalServiceType[Random.Shared.Next(0, EducationalServiceType.Length)]);
+        cct.Append(Random.Shared.Next(1, 9999).ToString("D4"));
+        cct.Append((char)Random.Shared.Next(65, 91));
+        return cct.ToString();
+    }
+}
