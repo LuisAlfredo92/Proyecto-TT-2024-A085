416f73dbe4229a86c550a7361132c902735c6d82
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-04-21 22:18:18 -0600

    Religion generator

---
+++ Data generators/Ideological data/Religion/ReligionGenerator.cs
@@ -0,0 +1,8 @@
+﻿using General_Data;
+
+namespace Ideological_data.Religion;
+
+public class ReligionGenerator
+{
+    public static string Generate() => StringGenerator.GenerateStringWithSpaces(Random.Shared.Next(8, 32));
+}
---
+++ Validation tests/Data generators/IdeologicalDataTests.cs
@@ -0,0 +1,19 @@
+﻿using Ideological_data.Religion;
+using Xunit.Abstractions;
+
+namespace Validation_tests.Data_generators;
+
+public class IdeologicalDataTests(ITestOutputHelper testOutputHelper)
+{
+    // Test religion generator
+    [Fact]
+    public void TestReligionGenerator()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var religion = ReligionGenerator.Generate();
+            Assert.InRange(religion.Length, 8, 32);
+            testOutputHelper.WriteLine(religion);
+        }
+    }
+}
