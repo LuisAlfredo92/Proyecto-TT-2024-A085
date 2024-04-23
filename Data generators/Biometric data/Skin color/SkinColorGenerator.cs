89dbbbcc927300d426e2e60965555ecd05021209
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-04-22 21:31:42 -0600

    Skin color generator

---
+++ Data generators/Biometric data/Skin color/SkinColorGenerator.cs
@@ -0,0 +1,8 @@
+﻿namespace Biometric_data.Skin_color;
+
+public class SkinColorGenerator
+{
+    public static byte GenerateFitzpatrick() => (byte)Random.Shared.Next(1, 7);
+
+    public static byte GeneratePerla() => (byte)Random.Shared.Next(1, 12);
+}
--- Validation tests/Data generators/BiometricalDataTests.cs
+++ Validation tests/Data generators/BiometricalDataTests.cs
@@ -1,4 +1,5 @@
 ﻿using Biometric_data.Iris;
+using Biometric_data.Skin_color;
 using Xunit.Abstractions;
 
 namespace Validation_tests.Data_generators;
@@ -12,5 +13,21 @@ public class BiometricalDataTests(ITestOutputHelper testOutputHelper)
         var filePath = IrisGenerator.Generate();
         Assert.True(File.Exists(filePath));
         testOutputHelper.WriteLine(filePath);
+    }
+
+    // Test skin color generator
+    [Fact]
+    public void TestSkinColorGenerator()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var fitzpatrick = SkinColorGenerator.GenerateFitzpatrick();
+            Assert.InRange(fitzpatrick, 1, 6);
+
+            var perla = SkinColorGenerator.GeneratePerla();
+            Assert.InRange(perla, 1, 11);
+
+            testOutputHelper.WriteLine($"{fitzpatrick} - {perla}");
+        }
     }
 }
