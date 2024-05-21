ec97abe0da9884e80858b1a9c9a5ac667a2af995
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-05-21 16:46:25 -0600

    Name FPE tests And fixing some typos

---
+++ Benchmark tests/Identifying data tests/Name tests/IdentifyingDataNameTwoFishFpeTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using FPE_ciphers;
+using Identifying_data.Names;
+
+namespace Name_tests;
+
+[MemoryDiagnoser]
+[MinColumn]
+[MeanColumn]
+[MedianColumn]
+[MaxColumn]
+[SimpleJob(launchCount: 100, iterationCount: 10)]
+public class IdentifyingDataNameTwoFishFpeTests
+{
+    private TwoFishFpe _twoFishFpe = null!;
+    private char[][] _names = null!;
+    private byte[]? _key;
+    private readonly char[] _alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz .ÁÉÍÓÚáéíóú".ToCharArray();
+
+    [GlobalSetup(Target = nameof(EncryptNameTwoFishFpe))]
+    public void SetupEncryption()
+    {
+        _key = new byte[32];
+        RandomNumberGenerator.Fill(_key);
+        _twoFishFpe = new TwoFishFpe(_key.AsSpan(), _alphabet);
+
+        var generatedName = NamesGenerator.Generate().ToCharArray().AsSpan();
+        var arrays = Math.Ceiling(generatedName.Length / 30f);
+        _names = new char[(int)arrays][];
+        for (var i = 0; i < _names.Length - 1; i++)
+            _names[i] = generatedName.Slice(i * 30, 30).ToArray();
+        _names[^1] = generatedName[((_names.Length - 1) * 30)..].ToArray();
+        var minLength = Math.Max(_names[^1].Length, 4);
+        Array.Resize(ref _names[^1], minLength);
+        for (var i = 0; i < _names[^1].Length; i++)
+        {
+            if (_names[^1][i] == '\0')
+                _names[^1][i] = ' ';
+        }
+    }
+
+    [Benchmark]
+    public char[][] EncryptNameTwoFishFpe()
+    {
+        var encryptedName = new char[_names.Length][];
+        for (var i = 0; i < _names.Length; i++)
+        {
+            encryptedName[i] = _twoFishFpe.Encrypt(_names[i]);
+        }
+
+        return encryptedName;
+    }
+
+    [GlobalSetup(Target = nameof(DecryptNameTwoFishFpe))]
+    public void SetupDecryption()
+    {
+        _key = new byte[32];
+        RandomNumberGenerator.Fill(_key);
+        _twoFishFpe = new TwoFishFpe(_key.AsSpan(), _alphabet);
+
+        var generatedName = NamesGenerator.Generate().ToCharArray().AsSpan();
+        var arrays = Math.Ceiling(generatedName.Length / 30f);
+        var names = new char[(int)arrays][];
+        for (var i = 0; i < names.Length - 1; i++)
+            names[i] = generatedName.Slice(i * 30, 30).ToArray();
+        names[^1] = generatedName[((names.Length - 1) * 30)..].ToArray();
+        var minLength = Math.Max(names[^1].Length, 4);
+        Array.Resize(ref names[^1], minLength);
+        for (var i = 0; i < names[^1].Length; i++)
+        {
+            if (names[^1][i] == '\0')
+                names[^1][i] = ' ';
+        }
+        _names = new char[(int)arrays][];
+        for (var i = 0; i < names.Length; i++)
+        {
+            _names[i] = _twoFishFpe.Encrypt(names[i]);
+        }
+    }
+
+    [Benchmark]
+    public char[][] DecryptNameTwoFishFpe()
+    {
+        var decryptedName = new char[_names.Length][];
+        for (var i = 0; i < _names.Length; i++)
+        {
+            decryptedName[i] = _twoFishFpe.Decrypt(_names[i]);
+        }
+        return decryptedName;
+    }
+}
--- Validation tests/Ciphers/FPE ciphers/AesFpeTests.cs
+++ Validation tests/Ciphers/FPE ciphers/AesFpeTests.cs
@@ -1,8 +1,11 @@
-﻿using FPE_ciphers;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using FPE_ciphers;
+using Xunit.Abstractions;
 
 namespace Validation_tests.Ciphers.FPE_ciphers;
 
-public class AesFpeTests
+public class AesFpeTests(ITestOutputHelper outputHelper)
 {
     private static readonly char[] Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
 
@@ -48,5 +51,52 @@ public class AesFpeTests
             decryptedData = aesFpe.Decrypt(cipherData);
 
         Assert.Equal(plainData, decryptedData);
+    }
+
+    [Fact]
+    public void TestTooLongInputs()
+    {
+        Span<byte> key = stackalloc byte[32];
+        var _alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz .ÁÉÍÓÚáéíóú".ToCharArray();
+        Span<char> plainData = stackalloc char[Random.Shared.Next(32, 150)];
+        ref var reference = ref MemoryMarshal.GetReference(plainData);
+        Random.Shared.NextBytes(key);
+        AesFpe aesFpe = new(key, _alphabet);
+
+        // Generate random name
+        for (var i = 0; i < plainData.Length; i++)
+        {
+            Unsafe.Add(ref reference, i) = _alphabet[Random.Shared.Next(_alphabet.Length)];
+        }
+        outputHelper.WriteLine($"Generated data: {plainData.ToString()}");
+
+        var arrays = Math.Ceiling(plainData.Length / 30f);
+        var names = new char[(int)arrays][];
+        for (var i = 0; i < names.Length - 1; i++)
+        {
+            names[i] = plainData.Slice(i * 30, 30).ToArray();
+        }
+        names[^1] = plainData[((names.Length - 1) * 30)..].ToArray();
+        var minLength = Math.Max(names[^1].Length, 4);
+        Array.Resize(ref names[^1], minLength);
+
+        for (var i = 0; i < names.Length; i++)
+        {
+            outputHelper.WriteLine($"Name {i}: {new string(names[i])}");
+        }
+
+        var cipherData = new char[(int)arrays][];
+        for (var i = 0; i < names.Length; i++)
+        {
+            cipherData[i] = aesFpe.Encrypt(names[i]);
+            outputHelper.WriteLine($"Encrypted name {i}: {new string(cipherData[i])}");
+        }
+
+        var decryptedData = new char[(int)arrays][];
+        for (var i = 0; i < cipherData.Length; i++)
+        {
+            decryptedData[i] = aesFpe.Decrypt(cipherData[i]);
+            outputHelper.WriteLine($"Decrypted name {i}: {new string(decryptedData[i])}");
+        }
     }
 }
