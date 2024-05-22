61e74792527afaf0820e17b7a7e370fe4fbbce54
LuisAlfredo92 <92luisalfredo@protonmail.com>
2024-05-21 19:17:46 -0600

    Password tests

---
+++ Benchmark tests/Digital data tests/Password tests/DigitalDataPasswordBCryptTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using Digital_data.Passwords;
+using Hashes;
+
+namespace Password_tests;
+
+[MemoryDiagnoser]
+[MinColumn]
+[MeanColumn]
+[MedianColumn]
+[MaxColumn]
+[SimpleJob(launchCount: 100, iterationCount: 10)]
+public class DigitalDataPasswordBCryptTests
+{
+    private readonly BCrypt _argon2Id = new();
+    private byte[] _password = null!;
+
+    [GlobalSetup(Target = nameof(EncryptPasswordBCrypt))]
+    public void SetupEncryption()
+    {
+        _password = Encoding.UTF8.GetBytes(PasswordsGenerator.GeneratePassword());
+    }
+
+    [Benchmark]
+    public Span<byte> EncryptPasswordBCrypt() => _argon2Id.Hash(_password);
+}
---
+++ Benchmark tests/Digital data tests/Password tests/DigitalDataPasswordSha2Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using Digital_data.Passwords;
+using Hashes;
+
+namespace Password_tests;
+
+[MemoryDiagnoser]
+[MinColumn]
+[MeanColumn]
+[MedianColumn]
+[MaxColumn]
+[SimpleJob(launchCount: 100, iterationCount: 10)]
+public class DigitalDataPasswordSha2Tests
+{
+    private byte[] _password = null!;
+
+    [GlobalSetup(Target = nameof(EncryptPasswordSha2))]
+    public void SetupEncryption()
+    {
+        _password = Encoding.UTF8.GetBytes(PasswordsGenerator.GeneratePassword());
+    }
+
+    [Benchmark]
+    public Span<byte> EncryptPasswordSha2() => Sha2.Hash(_password);
+}
